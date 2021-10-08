cd1a3e997883dd2aa6124db669b403a7081051a0
Akira Ohnishi <s06206ao@gmail.com>
2021-10-08 13:44:45 +0900

    Add hello()

--- Sources/MyFirstSwiftLibrary/MyFirstSwiftLibrary.swift
+++ Sources/MyFirstSwiftLibrary/MyFirstSwiftLibrary.swift
@@ -1,6 +1,9 @@
 public struct MyFirstSwiftLibrary {
     public private(set) var text = "Hello, World!"
 
-    public init() {
+    public init() {}
+
+    public func hello() -> String {
+        text
     }
 }
