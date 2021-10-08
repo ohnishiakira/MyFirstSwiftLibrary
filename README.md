da68b2815f9cca2d0cbfa7132a2fb73ce008256b
Akira Ohnishi <s06206ao@gmail.com>
2021-10-08 13:43:55 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyFirstSwiftLibrary
+
+A description of this package.
---
+++ Sources/MyFirstSwiftLibrary/MyFirstSwiftLibrary.swift
@@ -0,0 +1,6 @@
+public struct MyFirstSwiftLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
